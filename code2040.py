d21b3632f5ced27977560f38690e222fa9289cca
Raquel_Figueroa <raquelifigueroa@student.hartnell.edu>
2016-10-16 16:22:27 -0700

    modified request

--- code2040.py
+++ code2040.py
@@ -1,9 +1,12 @@
 import requests, json
 
-tokenString = "http://challenge.code2040.org/api/register"
+url = "http://challenge.code2040.org/api/register"
+tokenString = "be23993249a637699f148b0f39c889e0"
 gitString = "https://github.com/RaquelFigueroa/code2040Challenge"
 
 
 data = json.dumps({'token': tokenString, 'github': gitString})
 
-send = requests.post(tokenString, data)
+send = requests.post(url, data)
+
+print send.json
