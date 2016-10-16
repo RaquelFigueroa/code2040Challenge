69bda022935f71302759f2ca76950cf5c0e3dd97
Raquel_Figueroa <raquelifigueroa@student.hartnell.edu>
2016-10-16 16:48:02 -0700

    Deleted commented out code.

--- code2040.py
+++ code2040.py
@@ -4,9 +4,6 @@ url = "http://challenge.code2040.org/api/register"
 tokenString = "be23993249a637699f148b0f39c889e0"
 gitString = "https://github.com/RaquelFigueroa/code2040Challenge"
 
-
-#payload = json.dumps({'token': tokenString, 'github': gitString})
-
 payload = {'token': 'be23993249a637699f148b0f39c889e0', 'github': 'https://github.com/RaquelFigueroa/code2040Challenge'}
 
 r = requests.post(url , payload)
